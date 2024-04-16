aeba7f8aaa44c6e04a5a50f9d59843cd5bc627e2
sashatims@mail.ru <sashatims@mail.ru>
2024-04-16 13:41:36 +0300

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -23,59 +23,47 @@ import Image from 'next/image';
 export const Navbar = () => {
 	return (
 		<NextUINavbar maxWidth="xl" position="sticky" isBlurred={false}>
-			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-				<NavbarBrand as="li" className="gap-3 max-w-fit">
+				<NavbarBrand>
 					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<Image src="/logo.png" alt="logo" width={0} height={0} sizes="100vw" className="h-12 w-auto"/>
 					</NextLink>
 				</NavbarBrand>
+
+
+
+
+			<NavbarContent className="gap-8" justify="center">
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
 					{siteConfig.navItems.map((item) => (
-						<NavbarItem key={item.href}>
-							<NextLink
-								className={clsx(
-									linkStyles({ color: "foreground" }),
-									"data-[active=true]:text-primary data-[active=true]:font-medium !text-2xl mt-4"
-								)}
-								color="foreground"
-								href={item.href}
-							>
-								{item.label}
-							</NextLink>
-						</NavbarItem>
+							<NavbarItem key={item.href}>
+								<NextLink
+										className={clsx(
+												linkStyles({ color: 'foreground' }),
+												'data-[active=true]:text-primary data-[active=true]:font-medium !text-2xl mt-2',
+										)}
+
+										color="foreground"
+										href={item.href}
+								>
+									{item.label}
+								</NextLink>
+							</NavbarItem>
 					))}
 				</ul>
 			</NavbarContent>
 
 			<NavbarContent
-				className="hidden sm:flex basis-1/5 sm:basis-full"
-				justify="end"
+					className="hidden sm:flex"
+					justify="end"
 			>
-				<NavbarItem className="hidden sm:flex gap-2">
-					{/*<Link isExternal href={siteConfig.links.twitter} aria-label="Twitter">*/}
-					{/*	<TwitterIcon className="text-default-500" />*/}
-					{/*</Link>*/}
-					{/*<Link isExternal href={siteConfig.links.discord} aria-label="Discord">*/}
-					{/*	<DiscordIcon className="text-default-500" />*/}
-					{/*</Link>*/}
-					{/*<Link isExternal href={siteConfig.links.github} aria-label="Github">*/}
-					{/*	<GithubIcon className="text-default-500" />*/}
-					{/*</Link>*/}
-					{/*<ThemeSwitch />*/}
-				</NavbarItem>
-				{/*<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>*/}
-				<NavbarItem className="hidden md:flex">
-				{/*	<Button*/}
-        {/*    isExternal*/}
-				{/*		as={Link}*/}
-				{/*		className="text-sm font-normal text-default-600 bg-default-100"*/}
-				{/*		href={siteConfig.links.sponsor}*/}
-				{/*		startContent={<HeartFilledIcon className="text-danger" />}*/}
-				{/*		variant="flat"*/}
-				{/*	>*/}
-				{/*		Sponsor*/}
-				{/*	</Button>*/}
-				</NavbarItem>
+				<Button
+						isExternal
+						as={Link}
+						className="text-sm font-normal text-white bg-foreground2"
+						variant="flat"
+				>
+					JOIN US
+				</Button>
 			</NavbarContent>
 
 			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
@@ -89,7 +77,7 @@ export const Navbar = () => {
 			<NavbarMenu>
 				<div className="mx-4 mt-2 flex flex-col gap-2">
 					{siteConfig.navMenuItems.map((item, index) => (
-						<NavbarMenuItem key={`${item}-${index}`}>
+							<NavbarMenuItem key={`${item}-${index}`}>
 							<Link
 								color={
 									index === 2
